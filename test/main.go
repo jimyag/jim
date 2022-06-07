7c8908ba1fdf216fa5ae4f953445b641b8febc0d
Jimyag <jimyag@126.com>
2022-06-07 14:24:32 +0800

    feat: 添加前缀路由的main

--- test/main.go
+++ test/main.go
@@ -8,32 +8,29 @@ import (
 
 func main() {
 	r := jim.New()
-
-	// curl -i http://localhost:9999/
-	//HTTP/1.1 200 OK
-	//Content-Type: text/html
-	//Date: Sat, 04 Jun 2022 17:09:55 GMT
-	//Content-Length: 18
-	//
+	//curl http://localhost:9999
 	//<h1>Hello Jim</h1>
 	r.GET("/", func(c *jim.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Jim</h1>")
 	})
 
-	//curl "http://localhost:9999/hello?name=jimyag"
-	//hello jimyag, you're at /hello
+	// curl  http://localhost:9999/hello
+	//hello, you're at /hello
 	r.GET("/hello", func(c *jim.Context) {
-		// expect /hello?name=jimyag
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		c.String(http.StatusOK, "hello, you're at %s\n", c.Path)
 	})
 
-	//curl "http://localhost:9999/login" -X POST -d 'username=jimyag&password=jimyag'
-	//{"password":"jimyag","username":"jimyag"}
-	r.POST("/login", func(c *jim.Context) {
-		c.JSON(http.StatusOK, jim.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	// curl  http://localhost:9999/hello/jimyag
+	//hello jimyag, you're at /hello/jimyag
+	r.GET("/hello/:name", func(c *jim.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	// curl http://localhost:9999/assets/css/geektutu.css
+	//{"filepath":"css/geektutu.css"}
+	r.GET("/assets/*filepath", func(c *jim.Context) {
+		c.JSON(http.StatusOK, jim.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
