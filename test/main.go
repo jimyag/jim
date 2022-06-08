1c0cf204ca8e0719fefb40146a61b12d18ef5a88
Jimyag <jimyag@126.com>
2022-06-09 00:03:33 +0800

    feat: 分组路由的例子

--- test/main.go
+++ test/main.go
@@ -8,30 +8,38 @@ import (
 
 func main() {
 	r := jim.New()
-	//curl http://localhost:9999
-	//<h1>Hello Jim</h1>
-	r.GET("/", func(c *jim.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Jim</h1>")
+	r.GET("/index", func(c *jim.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
 
-	// curl  http://localhost:9999/hello
-	//hello, you're at /hello
-	r.GET("/hello", func(c *jim.Context) {
-		c.String(http.StatusOK, "hello, you're at %s\n", c.Path)
-	})
+	// curl http://localhost:9999/v1/hello?name=jimyag
+	//hello jimyag, you're at /v1/hello
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *jim.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Jim</h1>")
+		})
 
-	// curl  http://localhost:9999/hello/jimyag
-	//hello jimyag, you're at /hello/jimyag
-	r.GET("/hello/:name", func(c *jim.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *jim.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
 
-	// curl http://localhost:9999/assets/css/geektutu.css
-	//{"filepath":"css/geektutu.css"}
-	r.GET("/assets/*filepath", func(c *jim.Context) {
-		c.JSON(http.StatusOK, jim.H{"filepath": c.Param("filepath")})
-	})
+	// curl "http://localhost:9999/v2/hello/jimyag"
+	//hello jimyag, you're at /v2/hello/jimyag
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *jim.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *jim.Context) {
+			c.JSON(http.StatusOK, jim.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":9999")
 }
