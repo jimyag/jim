a59875c7f03a4274cec5216051b5c72f2a708ac4
Jimyag <jimyag@126.com>
2022-06-09 00:03:08 +0800

    feat: 添加分组路由

--- jim.go
+++ jim.go
@@ -1,6 +1,7 @@
 package jim
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -10,8 +11,10 @@ type HandleFunc func(ctx *Context)
 //
 // Engine 实现了 ServeHTTP 的接口
 type Engine struct {
+	*RouterGroup
 	// 保存路由和对应的处理方法
 	router *router
+	groups []*RouterGroup // 存储所有的 groups
 }
 
 //
@@ -20,7 +23,14 @@ type Engine struct {
 //  @return *Engine
 //
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{
+		router: newRouter(),
+	}
+	engine.RouterGroup = &RouterGroup{
+		engine: engine,
+	}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 //
@@ -78,4 +88,58 @@ func (engine *Engine) Run(addr string) error {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := newContext(w, r)
 	engine.router.handle(c)
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandleFunc // 支持中间件
+	parent      *RouterGroup // 支持嵌套
+	engine      *Engine      // 所有的 Groups 共享一个 Engine 实例
+}
+
+//
+// Group
+//  @Description: Group 定义为创建一个新的 RouterGroup
+// 记住所有 Group 共享相同的 Engine 实例
+//  @receiver group
+//  @param prefix
+//  @return *RouterGroup
+//
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandleFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+//
+// GET
+//  @Description:  GET defines the method to add GET request
+//  @receiver group
+//  @param pattern
+//  @param handler
+//
+func (group *RouterGroup) GET(pattern string, handler HandleFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+//
+// POST
+//  @Description: POST defines the method to add POST request
+//  @receiver group
+//  @param pattern
+//  @param handler
+//
+func (group *RouterGroup) POST(pattern string, handler HandleFunc) {
+	group.addRoute("POST", pattern, handler)
 }
