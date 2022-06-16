279a4a1047045d348389873a8cd494682134f4db
jimyag <jimyag@126.com>
2022-06-16 22:59:05 +0800

    feat:  添加路由的中间件

---
+++ logger.go
@@ -0,0 +1,18 @@
+package jim
+
+import (
+	"log"
+	"time"
+)
+
+// Logger
+func Logger() HandleFunc {
+	return func(c *Context) {
+		// Start timer
+		t := time.Now()
+		// Process request
+		c.Next()
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
